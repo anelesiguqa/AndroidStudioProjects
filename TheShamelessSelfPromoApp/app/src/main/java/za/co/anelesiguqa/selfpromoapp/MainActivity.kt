d8f4d63394e63f3c30a611214dd8c440cb1bd899
anelesiguqa <anelesiguqa@gmail.com>
2024-11-27 22:59:00 +0200

    Added TextInputLayout Added TextInputEditText Added Preview button, listener Added input types for inputs

--- TheShamelessSelfPromoApp/app/src/main/java/za/co/anelesiguqa/selfpromoapp/MainActivity.kt
+++ TheShamelessSelfPromoApp/app/src/main/java/za/co/anelesiguqa/selfpromoapp/MainActivity.kt
@@ -1,10 +1,13 @@
 package za.co.anelesiguqa.selfpromoapp
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +19,19 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val preview: Button = findViewById(R.id.button_preview);
+        preview.setOnClickListener {
+            onPreviewClicked()
+        }
     }
+
+    private fun onPreviewClicked() {
+        val contactNameEditText: TextInputEditText = findViewById(R.id.edit_text_contact_name);
+        val contactNumberEditText: TextInputEditText = findViewById(R.id.edit_text_contact_number);
+        val testString = contactNameEditText.text.toString() + ", " + contactNumberEditText.text.toString();
+
+        Toast.makeText(this, testString, Toast.LENGTH_LONG).show()
+    }
+
 }
